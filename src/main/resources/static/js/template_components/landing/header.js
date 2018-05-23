7adac6beb56b53c421c5a48a2c878fd1b1b9c4ca
Syed Mainul Hasan <mainuls18@gmail.com>
2018-05-23 12:47:50 +0600

    react preset es2015 was replaced by react preset env

--- src/main/resources/static/js/template_components/landing/header.js
+++ src/main/resources/static/js/template_components/landing/header.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = {
+	root: {
+		flexGrow: 1,
+	},
+	flex: {
+		flex: 1,
+	},
+	menuButton: {
+		marginLeft: -12,
+		marginRight: 20,
+	},
+};
 
 export class FrontEndHeader extends React.Component {
 	constructor(props) {
@@ -10,9 +31,17 @@ export class FrontEndHeader extends React.Component {
 	render() {
 		return (
 			<div>
-				<Button variant="raised" color="primary">
-					Hello World
-				</Button>
+				<AppBar position="static">
+					<Toolbar>
+						<IconButton className={styles.menuButton} color="inherit" aria-label="Menu">
+							<MenuIcon />
+						</IconButton>
+						<Typography variant="title" color="inherit" className={styles.flex}>
+							Title
+						</Typography>
+						<Button color="inherit">Login</Button>
+					</Toolbar>
+				</AppBar>
 			</div>
 		);
 	}
