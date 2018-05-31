9a55154db7552da1ea3c0fafdb34a88821b3dcb0
Syed Mainul Hasan <mainuls18@gmail.com>
2018-06-01 01:34:33 +0600

    index design committed

--- src/main/resources/static/js/components/landing/header.js
+++ src/main/resources/static/js/components/landing/header.js
@@ -11,27 +11,50 @@ import '../../../styles/base/_base.scss';
 class FrontEndHeader extends React.Component {
 	constructor(props) {
 		super(props);
-		 this.onSelectMenuItem = this.onSelectMenuItem.bind(this);
+		this.onSelectMenuItem = this.onSelectMenuItem.bind(this);
 	}
 
-	onSelectMenuItem(e){
-		document.querySelector(".ant-menu-item-selected").classList.remove("ant-menu-item-selected");
-		e.target.classList.add("ant-menu-item-selected");
+	onSelectMenuItem(e) {
+		let oldSelectedItem = document.querySelector('.ant-menu-item-selected');
+
+		if (oldSelectedItem) {
+			oldSelectedItem.classList.remove('ant-menu-item-selected');
+		}
+
+		if (e.target.tagName == 'I') {
+			e.target.parentElement.classList.add('ant-menu-item-selected');
+		} else {
+			e.target.classList.add('ant-menu-item-selected');
+		}
 	}
 
 	render() {
 		return (
 			<Layout>
 				<Header className="header">
-					<div className="logo" />
+					<div className="logo" style={{ float: 'left' }}>
+						ChainService
+					</div>
 
 					<div style={{ float: 'right' }}>
 						<ul
 							className="ant-menu ant-menu-dark ant-menu-root ant-menu-horizontal"
 							role="menu"
-							style={{"lineHeight": "64px"}}
+							style={{ lineHeight: '64px' }}
 						>
-							<li onClick={this.onSelectMenuItem} className="ant-menu-item right ant-menu-item-selected">
+							<li onClick={this.onSelectMenuItem} className="ant-menu-item right">
+								<Icon type="appstore" />
+							</li>
+						</ul>
+					</div>
+
+					<div style={{ float: 'right', marginRight: '10%' }}>
+						<ul
+							className="ant-menu ant-menu-dark ant-menu-root ant-menu-horizontal"
+							role="menu"
+							style={{ lineHeight: '64px' }}
+						>
+							<li onClick={this.onSelectMenuItem} className="ant-menu-item right">
 								Log in
 							</li>
 							<li onClick={this.onSelectMenuItem} className="ant-menu-item right">
